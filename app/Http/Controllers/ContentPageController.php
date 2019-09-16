db48d5ebe504267c7896e59d1fc4676d05596dab
noelkalel <noel.bg@hotmail.com>
2019-09-16 12:02:03 +0200

    first commit

---
+++ app/Http/Controllers/ContentPageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ContentPage;
+
+class ContentPageController extends Controller
+{
+    public function addContent($contentPage, $prefix, $request)
+    {
+        $contentPage->id= $prefix; 
+        $contentPage->user_id = auth()->id();
+        $contentPage->assets= $request->input('gjs-assets');
+        $contentPage->css= $request->input('gjs-css');
+        $contentPage->styles= $request->input('gjs-styles');
+        $contentPage->html= $request->input('gjs-html');
+        $contentPage->components= $request->input('gjs-components');
+
+        $contentPage->save();
+    }
+
+    public function store(Request $request)
+    {
+        //prefix = what is contained in main div (ID) -> what makes it a unique identifier
+        $prefix = $request->route('id');
+
+
+        $entry = ContentPage::where('id', '=', $prefix)->first();
+        if ($entry === null) {
+            //create new entry
+            $contentPage= new ContentPage();
+        } else {
+            $contentPage= $entry;
+        }
+        Self::addContent($contentPage, $prefix, $request);
+        return redirect()->back();
+    }
+
+    public function load(Request $request)
+    {
+        $prefix = request('id');
+
+        $contentPage = ContentPage::whereId($prefix)->first(); 
+
+        if ($contentPage === null) {
+            $contentPage = new ContentPage();
+            Self::addContent($contentPage, $prefix, $request);
+        }
+
+        $labels = ['assets', 'css', 'styles', 'html', 'components', ]; 
+
+        $json = [
+            'id' => request('id'),
+            'gjs-assets' =>  $contentPage->getAttribute('assets'),
+            'gjs-css' =>  $contentPage->getAttribute('css'),
+            'gjs-styles' =>  $contentPage->getAttribute('styles'),
+            'gjs-html' =>  $contentPage->getAttribute('html'),
+            'gjs-components' =>  $contentPage->getAttribute('components')
+        ];
+
+        header('Content-Type: application/json');
+
+        return json_encode($json);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index-simple');
+});
+
+Route::post('/content/store/{id}', 'ContentPageController@store')->name('content.store');
+Route::get('/content/load/{id}', 'ContentPageController@load')->name('content.load');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
